6d8588f7a05964e5a2c5cf444fd422c5c6ac32e2
David Dupre <djdupre@uw.edu>
2018-08-11 18:14:32 -0700

    Slightly improve servo motor

--- bot_ross/servo_motor.py
+++ bot_ross/servo_motor.py
@@ -14,17 +14,20 @@ class ServoMotor():
         bus.write_byte_data(self.addr, 0, 0x20)
         bus.write_byte_data(self.addr, 0xfe, 0x1e)
 
-    def do_something(self):
+    def set_raw(self, value):
         """
         Not sure what this actually does. Copied from here:
         https://learn.sparkfun.com/tutorials/pi-servo-hat-hookup-guide#software---python
+
+        value {int} -- from 0 to 4095
         """
         # Write data to PWM chip
         bus.write_word_data(self.addr, 0x06, 0)
-        bus.write_word_data(self.addr, 0x08, 1250)
+        bus.write_word_data(self.addr, 0x08, value)
 
 
 if __name__ == '__main__':
     bus = SMBus(1)  # using i2c bus 1
     servo_motor = ServoMotor(bus, addr=0x40)
     servo_motor.do_something()
+    bus.close()
